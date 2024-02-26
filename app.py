ab733a5983555d5f5f3bb2d932f347de3e0ab8fe
kubadusza <jacob.dusza@gmail.com>
2024-02-26 14:52:49 +0100

    initial commit

---
+++ app.py
@@ -0,0 +1,131 @@
+import streamlit
+
+from imports import *
+from modules.setup import setup, grant_access
+from modules.page import sticky_header, display_chat_buttons
+from modules.chat import display_chat, ask_question
+from constants import *
+
+import pandas as pd
+
+
+def display_notifications(notifications):
+    """
+    Displays notifications in Streamlit with corresponding icons.
+
+    Parameters:
+    - notifications: A dictionary with keys 'error', 'info', 'warning', and 'success',
+                     where each key maps to a string message to be displayed.
+    """
+    if notifications is None:
+        return
+
+    if 'info' in notifications:
+        st.info(notifications['info'], icon="ℹ️")
+
+    if 'success' in notifications:
+        st.success(notifications['success'], icon="✅")
+
+    if 'warning' in notifications:
+        st.warning(notifications['warning'], icon="⚠️")
+
+    if 'error' in notifications:
+        st.error(notifications['error'], icon="🚨")
+
+
+def main():
+    col1, col2 = st.columns([1, 1])
+    notifications = None
+
+
+
+    with st.sidebar:
+
+        st.markdown("""
+        # Pharmacy Assistant Chatbot Capabilities
+        
+This demo was made to showcase the possibilities of use of the chatbot.
+key features:
+
+- **Serialized Record Data Interactions**: Retrieve or modify record data
+- **Text, Unorganized Data Interactions**: retrieve information from unprocessed, already avaliable data, like pharmacy webpage
+- **Pharmacist Notifications**: notify pharmacists with updates
+- **Warning Notification System**: Sends alerts in response to critical messages for immediate attention
+        """)
+
+        with st.expander("System Prompt"):
+            system_prompt = st.text_area(label="write a system message, that tells the model how to act",
+                                         value=WHOAMI,
+                                         height=350)
+            st.session_state.whoami = system_prompt
+
+        with st.expander("Things to try"):
+            st.write("1. add behaviour to the chatbot by inserting some requirement to the system message, like:\n"
+                     "  - act like a duck, by adding *quack!* to your sentences\n\n"
+                     "2. Try asking for the data in the patients records or from the text information about the pharmacy\n\n"
+                     "3. Ask it to add new or change existing record data, like changing the age, scheduling an appointment, taking a note, or saying, that you are allergicn\n\n"
+                     "4. Ask it to notify the pharmacist about something\n\n"
+                     "5. Send a worrying message on the chat, like:\n\n "
+                     "'i think i overdosed' to test the warning notification system")
+
+
+
+    with col1:
+        display_chat()
+
+
+
+
+
+    notifications = ask_question(col1)
+
+    with col1:
+        display_chat_buttons()
+
+    with col2:
+        st.header("Example Record Data")
+        edited_record_data = st.data_editor(data=st.session_state.pharmacy_example_record_data,
+                                num_rows="dynamic",
+                                column_config={
+                                    "Allergic to XYZ": st.column_config.CheckboxColumn(
+                                        label="Allergic to XYZ",
+                                        help="Select your **favorite** widgets",
+                                        default=False,
+                                    ),
+
+
+                                }
+                                )
+
+        st.session_state.pharmacy_example_record_data = edited_record_data
+
+
+        st.header("Example Text Data")
+        editable_text_data = st.text_area("",
+                                          height=500,
+                                          value=PHARMACY_EXAMPLE_TEXT_INFO)
+
+        st.session_state.pharmacy_example_text_info = editable_text_data
+
+
+        #notifications
+        display_notifications(notifications)
+        #if notifications is not None:
+
+
+    # with st.sidebar:
+    # display_pdfs()
+
+
+if __name__ == '__main__':
+    load_dotenv()
+    setup()
+
+    sticky_header()
+
+    # st.write(st.session_state.access_key in allowed_access_keys)
+    # st.write("allowed access keys:", allowed_access_keys)
+    # st.write("access key:", st.session_state.access_key)
+
+    if grant_access():
+        main()
---
+++ modules/chat.py
@@ -0,0 +1,289 @@
+import json
+import stat
+
+import streamlit
+
+from imports import *
+from constants import *
+from utils.emotion_classification import EmotionClassifier
+openai.api_key = st.secrets["OPENAI_API_KEY"]
+
+
+@st.cache_resource(show_spinner=False)
+def get_emotion_classifier():
+    return EmotionClassifier()
+
+
+TOOLS = [{
+    "type": "function",
+    "function": {
+        "name": "manipulate_dataframe",
+        "description": "Use only when asked to manipulate a record by default do not use it. Modifies a specified field in a DataFrame by setting it to a new value and provides a response to the user.",
+        "parameters": {
+            "type": "object",
+            "properties": {
+                "response": {
+                    "type": "string",
+                    "description": "The response to the user before the operation is performed, informing, what action is being performed, but shortly."
+                },
+                "row": {
+                    "type": "integer",
+                    "description": "The index (row number) of the DataFrame where the change is to be applied."
+                },
+                "column": {
+                    "type": "string",
+                    "description": "The name of the column in the DataFrame where the change is to be applied."
+                },
+                "new_value": {
+                    "type": "string",
+                    "description": "The new value to set for the specified field (cell) in the DataFrame."
+                }
+
+            },
+            "required": ["response", "dataframe", "row", "column", "new_value"]
+        }
+    }
+    },
+    {
+      "type": "function",
+      "function": {
+        "name": "notify_pharmacist",
+        "description": "Sends a notification message to the pharmacist and provides a predetermined response to the user to confirm the action.",
+        "parameters": {
+          "type": "object",
+          "properties": {
+            "response": {
+              "type": "string",
+              "description": "The response to the user after the notification is sent, informing them about the action taken in a brief manner."
+            },
+            "message": {
+              "type": "string",
+              "description": "The message to be sent to the pharmacist. This should be clear and concise, providing all necessary information."
+            },
+            "notification_type": {
+              "type": "string",
+              "enum": ["warning", "info"],
+              "description": "The type of notification to send. Can be either 'warning' for urgent matters or 'info' for informational messages."
+            }
+
+          },
+          "required": ["message", "response", "notification_type"]
+        }
+      }
+    }
+
+]
+def get_response(messages, model = None, stream = True, temperature = 1.0, tools = None):
+    if model is None:
+        model = st.session_state["openai_model"]
+
+    return  st.session_state.openai_client.chat.completions.create(
+        model=model,
+        messages=messages,
+        stream=stream,
+        temperature=temperature,
+        tools = tools)
+
+
+def display_chat():
+
+    for message in st.session_state.messages:
+        with st.chat_message(message["role"], avatar=message["avatar"]):
+            st.markdown(message["content"])
+            if message.get("function_call") is not None:
+                st.markdown(message["function_call"])
+
+
+def ask_question(container):
+    # prompt = st.session_state.prompt
+    notifications = None
+
+    prompt = st.chat_input("Ask a question")  # , key="prompt")
+
+    st.session_state.prompts.append(prompt)
+
+    if st.session_state.regenerate:
+        last_non_null_text = next((text for text in reversed(st.session_state.prompts) if text is not None), None)
+
+        prompt = last_non_null_text
+        st.session_state.regenerate = False
+
+
+    with container:
+
+        if prompt:
+            avatar = get_emotion_classifier().classify(prompt)
+
+            with st.chat_message("user", avatar=avatar):
+                st.markdown(prompt)
+
+            st.session_state.messages.append({"role": "user", "content": prompt, "avatar": avatar})
+
+            json_string_record_data = st.session_state.pharmacy_example_record_data.to_json(orient='records')
+
+            docs = [st.session_state.pharmacy_example_text_info, json_string_record_data]
+
+            # st.write(docs)
+
+            notifications = ai_message(docs)
+
+            x = '''any_expanded = False
+            for i, column in enumerate(columns):
+                if not any_expanded:
+                    with column:
+                        with st.expander(docs[i].page_content[:10] + "..."):
+                            any_expanded = True
+                            write_atsize(docs[i].page_content, 12)'''
+    return notifications
+
+
+def notify_pharmacist(function_arguments):
+    response = function_arguments['response']
+    notification_type = function_arguments['notification_type']
+    notification = {notification_type: function_arguments['message']}
+    return response, notification
+
+def hanhandle_function_calls(function_name, function_arguments_json):
+    response = ""
+    notifications = None
+    if function_name == "manipulate_dataframe":
+        st.session_state.pharmacy_example_record_data, response, notifications = manipulate_dataframe(
+            dataframe=st.session_state.pharmacy_example_record_data, args=function_arguments_json)
+
+    if function_name == "notify_pharmacist":
+        response, notifications = notify_pharmacist(function_arguments=function_arguments_json)
+
+
+    return response, notifications
+
+
+def manipulate_dataframe(dataframe, args):
+    """
+    Updates a specified cell in a pandas DataFrame.
+
+    Parameters:
+    - dataframe: A pandas DataFrame to be modified.
+    - args: A dictionary containing the 'response', 'row', 'column', and 'new_value' keys.
+
+    Returns:
+    - A tuple containing the updated DataFrame and the response message.
+    """
+    # Extract arguments
+    response = args['response']
+    row = args['row']
+    column = args['column']
+    new_value = args['new_value']
+
+    # Check if the specified column exists in the DataFrame
+    if column not in dataframe.columns:
+        return dataframe, response,  {"error": "Column not found."}
+
+    # Check if the specified row index is within the DataFrame's range
+    if row >= len(dataframe) or row < 0:
+        return dataframe, response, {"error": "Row index out of range."}
+
+    # Update the DataFrame
+    dataframe.at[row, column] = new_value
+
+    print(dataframe)
+
+    # Return the updated DataFrame and the response message
+    return dataframe, response, {"success": f" succesfully changed field [{row},{column}] to {new_value}"} #df, response, notificaion
+
+
+def ai_message(docs):
+    notifications = None
+
+    INSTRUCTION_MESSAGE = {
+        "role": "system",
+        "content": SYSTEM_MESSAGE.format(ASSISTANT_IDENTITY = st.session_state.whoami)
+    }
+
+    if docs:
+        relevant_docs_messages = [{"role": "assistant", "content": "content of document fragment:\n" + doc} for doc in docs]
+    else:
+        relevant_docs_messages = [{"role": "system", "content": "there are no available documents from the user"}]
+
+    chat_history_header = [
+        {"role": "system", "content": "### end of relevant documents. the next messages contain the chat history ###"}]
+
+    with (st.chat_message("assistant", avatar=MAIN_ICON)):
+        functioncall_placeholder = st.empty()
+        message_placeholder = st.empty()
+
+        full_response = ""
+        messages = [INSTRUCTION_MESSAGE] + relevant_docs_messages + chat_history_header + [
+            {"role": m["role"], "content": m["content"]} for m in
+            st.session_state.messages[-SLIDING_CHAT_WINDOW_SIZE:]]
+
+        function_name = None
+        function_arguments = None
+
+        # Flag to indicate if the function name has been captured
+        function_name_captured = False
+        detected_tool_call = False
+
+        for chunk in get_response(messages, tools=TOOLS):
+            chunk = chunk.choices[0].delta
+
+            if chunk.tool_calls:
+                detected_tool_call = True
+                # If we haven't captured the function name yet, get it from the first tool call
+                if not function_name_captured:
+                    function_name = chunk.tool_calls[0].function.name
+                    if function_name is not None:
+                        function_arguments = ""
+                        function_name_captured = True
+
+
+                # Iterate through each tool call in the chunk
+                for tool_call in chunk.tool_calls:
+                    # If the tool call has arguments, append them to the function_arguments string
+                    if tool_call.function.arguments:
+                        function_arguments += tool_call.function.arguments
+
+                functioncall_placeholder.markdown(f"```python\n"
+                                                  f"function_name: {function_name}, arguments: {function_arguments}\n"
+                                                  f"```")
+
+
+            else:
+                content = getattr(chunk, "content", "")
+                if content:
+                    full_response += content
+                #full_response += chunk.choices[0].delta.get("content", "")
+                message_placeholder.markdown(full_response + "▌")
+
+        if detected_tool_call:
+            try:
+                function_arguments_json = json.loads(function_arguments)
+            except json.JSONDecodeError:
+                # If JSON parsing fails, keep the raw string
+                function_arguments_json = function_arguments
+
+        if detected_tool_call:
+            functioncall_placeholder.markdown(f"```python\n"
+                                              f"function_name: {function_name}, arguments: {function_arguments}\n"
+                                              f"```")
+            full_response, notifications = hanhandle_function_calls(function_name, function_arguments_json=function_arguments_json)
+
+            #parsed_json = json.loads(function_arguments_json)
+
+
+        message_placeholder.markdown(full_response)
+
+
+    function_call_record = None
+
+    if function_name is not None:
+        function_call_record = f"```python\n"\
+                               f"function_name: {function_name}, arguments: {function_arguments}\n"\
+                               f"```"
+
+
+    st.session_state.messages.append({"role": "assistant", "content": full_response,"function_call":function_call_record, "avatar": MAIN_ICON})
+    return notifications
+
+
+
+REPHRASE_QUESTION_INSTRUCTION_TEXT = """###INSTRUCTIONS: Construct a query from the last question in the provided chat history, suitable for a similarity search in a vector store, using keywords and context from the history.You can also add other keywords you think will help. Respond only with the query, without any additional prefixes or labels.###"""
